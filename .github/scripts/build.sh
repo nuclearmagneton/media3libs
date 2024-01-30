1877ffc793346c013aa03a43f7fef184b2d82eba
nuclearmagneton <113956472+nuclearmagneton@users.noreply.github.com>
2024-01-30 14:18:03 +0530

    update

---
+++ .github/scripts/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eux
+
+echo $ANDROID_NDK_HOME
+echo $NDK_PATH
+
+chmod +x ${ROOT_DIR}/.github/scripts/build-ffmpeg.sh
+chmod +x ${ROOT_DIR}/.github/scripts/build-av1.sh
+
+cd media
+export MEDIA3_PATH="$(pwd)"
+
+
+${ROOT_DIR}/.github/scripts/build-ffmpeg.sh
+${ROOT_DIR}/.github/scripts/build-av1.sh
+cd ${MEDIA3_PATH}
+./gradlew publishToMavenLocal
+
+
+cd ${ROOT_DIR}
+mkdir -p repo
+mv ~/.m2/repository/* repo
+
+
+git config --local user.email "github-action@users.noreply.github.com"
+git config --local user.name "GitHub Action"
+git add repo/**
+git commit -m "build aar"
